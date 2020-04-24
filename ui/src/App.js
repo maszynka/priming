530583fe40173717a463b8e38f9069caba6e42a0
Daniel Bogusz <d.bogusz@bsgroup.eu>
2020-04-24 18:24:35 +0200

    working on questions

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState } from 'react';
+import { Input } from 'antd';
+
 import logo from './logo.svg';
 import './App.css';
 import AffectiveSlider from "./AffectiveSlider";
@@ -17,18 +19,30 @@ const questions = [
         p: 'Dobro'
     },
     {
-        question: 'Wiek',
+        question: 'Miasto',
         type: 'select',
-        answers: ['poniżej 18', '18-29', '30-39', '40-49', '50-60' , 'powyżej 60 roku'],
+        answers: ['do 50 tyś. mieszkańsów', 'między 50 a 100 tyś. mieszkańsów', 'między 100 a 250 tyś. mieszkańsów', 'powyżej 500 tyś. mieszkańsów'],
         p: 'Dobro'
-    },
-    {
-        question: 'Wiek',
-        type: 'select',
-        answers: ['poniżej 18', '18-29', '30-39', '40-49', '50-60' , 'powyżej 60 roku'],
-        p: 'Dobro'
-    },
-]
+    }
+];
+
+const Question = ({question, type, answers, p})=> {
+    const [placeholder, setPlaceholder] = useState(question);
+    const startFun = ()=>{
+        setPlaceholder(p);
+        setTimeout(()=>{
+            setPlaceholder(question);
+        }, 50)
+    };
+
+    useEffect(()=>{
+        console.log('fired')
+        setTimeout(startFun, 500)
+    }, []);
+    return <Input placeholder={placeholder}>
+
+    </Input>
+}
 
 function App() {
     useEffect(()=> {
@@ -39,6 +53,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          {questions.map(questionData => <Question {...questionData} key={questionData.question}/>)}
         <AffectiveSlider/>
       </header>
     </div>
